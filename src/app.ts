80d893a4111ab87886bd84958f2a5b93bb93523a
Kevin <326.osito@gmail.com>
2025-06-17 20:33:20 -0400

    feat: Auth module
    To authenticate login with jwt

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import userRoutes from './modules/users/users.routes';
+import authRoutes from './modules/auth/auth.routes';
+import taskRoutes from './modules/task/task.routes';
 
 const app = express();
 const prefix = '/api/v1';
@@ -10,6 +12,8 @@ app.use(express.json());
 
 // routes
 app.use(`${prefix}/users`, userRoutes); // users
+app.use(`${prefix}/login`, authRoutes); // auth
+app.use(`${prefix}/task`, taskRoutes); // task
 
 /* app.get('/', (req, res) => {
   res.send('Hola mundo!');
---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express';
+import { matchPassword } from '../../common/matchPassword';
+import jwt from 'jsonwebtoken';
+import { prisma } from '../../utils/prisma/prismaClient';
+import { config } from '../../utils/config/env';
+import { userLogin } from './auth.service';
+
+export const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  const user = await userLogin(username);
+  const isMatch = await matchPassword(password, user!.password);
+
+  if (!isMatch) {
+    res.status(401).json({
+      message: 'Invalid credentials',
+    });
+  }
+  const secretKey = config.SECRET_KEY;
+  // DO: preguntar ing
+  // const expires = config.EXPIRES_IN || '1h';
+  const token = jwt.sign(
+    {
+      userId: user!.id,
+      status: user!.status,
+    },
+    secretKey,
+    {
+      expiresIn: '1h',
+    },
+  );
+
+  res.json({
+    token,
+  });
+};
