f824db43eee00444fea40381e1f947e6f56d35ae
Kevin <326.osito@gmail.com>
2025-06-19 00:01:59 -0400

    fix: some configurations

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import app from './app';
 import { config } from './utils/config/env';
 import logger from './utils/logs/logger';
 
-// const PORT = config.port;
 const port = config.PORT;
 
 const main = () => {
